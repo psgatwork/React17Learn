684b1d48b47c6b9f704286bb56b549fa7c677949
PrabathShanakaGurusinghe <psgatwork@gmail.com>
2022-08-28 12:17:21 +0530

    Added header component.

---
+++ src/main-page/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Component } from "react";
+import logo from "./GloboLogo.png";
+
+const Header = () => (
+  <header className="row">
+    <div className="col-md-5">
+      <img src={logo} className="logo" alt="logo" />
+    </div>
+    <div className="col-md-7 mt-5 subtitle">
+      Providing houses all over the world
+    </div>
+  </header>
+);
+
+export default Header;
