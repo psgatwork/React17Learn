2bee548b56fe539e9d3ae3be25f09f0c7db84a5a
PrabathShanakaGurusinghe <psgatwork@gmail.com>
2022-09-04 22:39:59 +0530

    Hooks: useEfects 5.6

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import Header from './header';
 import './main-page.css';
 
 function App() {
+  let houseCollection;
+  useEffect(()=>{
+    const fetchHouses = async () => {
+      const rsp = await fetch("/houses.json");
+      const houses = await rsp.json();
+      return houses;
+    };
+    fetchHouses().then((data) =>{
+      console.log('use effect data fetch', data);
+    });
+    console.log('use effect fired');
+  },[]);
   return (
     <div className='Container'>
       <Header subtitle="House List - All" title="list of houses for sale"></Header>
